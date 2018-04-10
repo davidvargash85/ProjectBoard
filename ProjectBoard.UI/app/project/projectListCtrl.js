8902360c0fd3f8e6a1c8194886c1bd2cefbab620
David Vargas <davidvargash@gmail.com>
2018-04-09 20:22:16 -0400

    Add project files.

---
+++ ProjectBoard.UI/app/project/projectListCtrl.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("projectBoard")
+        .controller("projectListCtrl", [ProjectListCtrl]);
+
+    function ProjectListCtrl() {
+        var vm = this;
+    }
+}());
