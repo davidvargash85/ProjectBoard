725c674b34cf45a75d73dd4a7a0e46dfd144d7f6
David Vargas <davidvargash@gmail.com>
2018-06-23 12:27:55 -0400

    updating project view

---
+++ ProjectBoard.UI/app/stage/stageAddEditCtrl.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("projectBoard")
+        .controller(
+        "stageAddEditCtrl",
+        [
+            "stage",
+            "$state",
+            StageAddEditCtrl
+        ]);
+
+    function StageAddEditCtrl(stage, $state, $log) {
+        var vm = this;
+
+        vm.stage = stage;
+
+        vm.title = vm.stage.id ? "Edit Stage: " + vm.stage.name : "Add Stage";
+
+        vm.submit = function (isValid) {
+            if (isValid) {
+                //TODO: check on Error for both promises
+                if (vm.project.id) {
+                    
+                }
+                else {
+                    
+                }
+            } else {
+                alert("Please correct the validation errors first.");
+            }
+        };
+
+        vm.cancel = function () {
+            
+        };
+
+    }
+
+
+}());
---
+++ ProjectBoard.UI/common/stageResource.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("stageResource",
+        ["$stage",
+        "appSettings",
+        stageResource]);
+
+    function stageResource($stage, appSettings) {
+        return $resource(appSettings.serverPath + "/api/projects/:idProject/stages/:idStage", null,
+            {
+                'update': { method: 'PUT' }
+            });
+    }
+}());
